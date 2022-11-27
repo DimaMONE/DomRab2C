c5f66a2c8ecb0ef87a5e79239b23ad7b9023484e
SmaginDima <smaginzdima@gmail.com>
2022-11-28 00:28:57 +0300

    вторая задача

---
+++ n2sem2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 13: Выводит третью цифру заданного числа или сообщает, что третьей цифры нет
+Console.WriteLine("Введите целое число");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a < 100)
+{
+    Console.WriteLine("Третьей цифры слева нет");
+}
+
+else
+{
+    while (a > 999)
+    {
+        a = a / 10;
+    }
+    int d = a % 10 % 10;
+    Console.WriteLine($"Третья цифра слева равна {d}");
+}
