bedc4d721d374d8e3d56b46a1f1ba2034754e363
SmaginDima <smaginzdima@gmail.com>
2022-11-29 20:21:13 +0300

    третья задача

---
+++ n3sem2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Выводит четные числа от 1 до N
+Console.WriteLine("Привет Тимур");
+int i = 1;
+for (i = 1; i < 101; i++)
+{
+    if (i % 15 == 0)
+    {
+        Console.WriteLine("FIZZ BUZZ");
+    }
+    else if (i % 3 == 0)
+    {
+        Console.WriteLine("FIZZ");
+    }
+    else if (i % 5 == 0)
+    {
+        Console.WriteLine("BUZZ");
+    }
+    else
+    {
+        Console.WriteLine(i);
+    }
+}
+Console.WriteLine();
